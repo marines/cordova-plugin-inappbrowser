3794dd638d8779cced7d887b292fdea06d7bbe81
Mariusz Kujawski <mariusz@kujaw.ski>
2017-08-27 13:04:54 +0200

    auth0 exception

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -846,6 +846,10 @@ public class InAppBrowser extends CordovaPlugin {
                 } catch (android.content.ActivityNotFoundException e) {
                     LOG.e(LOG_TAG, "Error sending sms " + url + ":" + e.toString());
                 }
+            } else if (url.startsWith('auth0:')) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                view.getContext().startActivity( intent );
+                return true;
             }
             return false;
         }
